bb272d793607fc33256f494fd7d43c2e569e6ff9
James Batsell <jamesbatsell05@gmail.com>
2020-04-10 04:01:52 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,11 +4,9 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters
 
   def actors_list
-    actors = []
-    first_name = self.actors.map {|actor| actor.first_name}
-    last_name = self.actors.map {|actor| actor.last_name}
-    combine = first_name.concat(last_name)
-    actors << combine.join(" ")
-    actors
+    def actors_list
+    list =[]
+    self.actors.map{|a| list << "#{a.full_name}"}
+    list
   end
 end
